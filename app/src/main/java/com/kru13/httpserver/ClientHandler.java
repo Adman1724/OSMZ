1fc2f93a76e8194abf6f8a56dd11d46df2952af3
Adman1724 <adam.majak2@gmail.com>
2020-03-30 18:58:26 +0200

    no message

---
+++ app/src/main/java/com/kru13/httpserver/ClientHandler.java
@@ -0,0 +1,317 @@
+package com.kru13.httpserver;
+
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class ClientHandler extends Thread implements Camera.PreviewCallback {
+
+    public static String NEWLINE = "\r\n";
+    private ServerSocket serverSocket;
+    private boolean bRunning = false;
+
+    private Handler messageHandler;
+    String boundary = "--boundary";
+    boolean streaming = false;
+
+    CameraManager cameraManager;
+    Camera cameraInstance;
+    DataOutputStream stream;
+    ByteArrayOutputStream imageBuffer;
+
+
+    public ClientHandler(ServerSocket socket, Handler handler, CameraManager cameraManager, Camera cameraInstance) {
+        this.serverSocket = socket;
+        this.messageHandler = handler;
+        this.cameraManager = cameraManager;
+        this.cameraInstance = cameraInstance;
+        cameraInstance.setPreviewCallback(this);
+    }
+
+    public void run() {
+
+        try {
+
+            bRunning = true;
+            while (bRunning) {
+
+                Log.d("SERVER", "Socket Waiting for connection");
+                Socket s = serverSocket.accept();
+                //(new ClientHandler(serverSocket,messageHandler, cameraManager, cameraInstance)).start();
+                Log.d("SERVER", "Socket Accepted");
+
+                OutputStream o = s.getOutputStream();
+                BufferedWriter res = new BufferedWriter(new OutputStreamWriter(o));
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String req = in.readLine();
+                String[] reqData = req.split("\\s+");
+                String reqestType = reqData[0];
+                String requestedFile = reqData[1];
+                String resMessage="";
+                if (reqestType.equals("GET")) {
+                    File outFile = null;
+                    if (requestedFile.contains("/stream")) {
+                        stream = new DataOutputStream(s.getOutputStream());
+                        if (stream != null) {
+                            try {
+                                stream.write(("HTTP/1.0 200 OK\r\n" +
+                                        "Server: localhost/12345" + NEWLINE +
+                                        "Cache-Control:  no-cache" + NEWLINE +
+                                        "Cache-Control:  private" + NEWLINE +
+                                        "Content-Type: multipart/x-mixed-replace;boundary=" + boundary + NEWLINE).getBytes());
+
+                                stream.flush();
+
+                                streaming = true;
+                                imageBuffer = new ByteArrayOutputStream();
+                            } catch (IOException e) {
+                                Log.d("ERROR:", e.getLocalizedMessage());
+                                streaming = false;
+                            }
+                        }
+
+                    }
+                    else if(requestedFile.contains("/cgi-bin")){
+                        String commands[] = requestedFile.split("/");
+
+                        if (commands.length < 3)
+                            break;
+
+                        String command = commands[2];
+
+                        try{
+                            Process process = Runtime.getRuntime().exec(command);
+                            BufferedReader bufferedReader = new BufferedReader(
+                                    new InputStreamReader(process.getInputStream()));
+
+                            res.write("HTTP/1.0 200 Ok" + NEWLINE);
+                            res.write("Date: " + Calendar.getInstance().getTime() + NEWLINE);
+//                                out.write("Content-Length: " + String.valueOf(bufferedReader) + NEWLINE);
+                            res.write("Content-Type: text/html" + NEWLINE);
+                            res.write(NEWLINE);
+                            res.write("<html>");
+                            String line;
+                            while ((line = bufferedReader.readLine()) != null){
+                                res.write("<h3>" + line + "</h3>");
+                            }
+                            res.write(NEWLINE);
+
+                            res.write("</html>");
+                            res.flush();
+
+                        }
+                        catch (Exception e){
+                            Log.d("ProcessOutput", "just failed: " + e.getMessage());
+
+                        }
+                        Message msg = messageHandler.obtainMessage();
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("REQUEST", (Serializable) resMessage);//wtf
+                        msg.setData(bundle);
+                        messageHandler.sendMessage(msg);
+
+
+                        s.close();
+                        Log.d("SERVER", "Socket Closed");
+
+                    }
+                    else {
+                        if (reqData[1].equalsIgnoreCase("/")) {
+
+
+                            outFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "index.html");
+                            res.write("HTTP/1.1 200 OK" + NEWLINE);
+
+                            res.write("Server: localhost/12345" + NEWLINE);
+                            res.write("Content-Length: " + String.valueOf(outFile.length()) + NEWLINE);
+                            res.write("Connection: Closed" + NEWLINE);
+                            res.write("Content-Type: text/html"+ NEWLINE);
+
+
+                            res.write(NEWLINE);
+                            res.flush();
+                            byte[] buf = new byte[1024];
+                            int len = 0;
+                            FileInputStream fis = new FileInputStream(outFile);
+
+                            while ((len = fis.read(buf)) > 0) {
+                                o.write(buf, 0, len);
+                            }
+                            resMessage=outFile.length()+";"+requestedFile+";"+s.getRemoteSocketAddress().toString();
+
+
+                        }
+                        else if (requestedFile.contains("/webcam?")) {
+                            makeCameraPhoto();
+                            outFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "/photos/camera.jpg");
+                        } else {
+                            outFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), requestedFile);
+                        }
+                        if (outFile.exists()) {
+
+                            res.write("HTTP/1.1 200 OK" + NEWLINE);
+                            res.write("Date: " + Calendar.getInstance().getTime() + NEWLINE);
+                            res.write("Server: localhost:12345" + NEWLINE);
+                            res.write("Content-Length: " + String.valueOf(outFile.length()) + NEWLINE);
+                            res.write("Connection: Closed" + NEWLINE);
+                            res.write(NEWLINE);
+                            res.flush();
+
+                            byte[] buf = new byte[1024];
+                            int len = 0;
+                            FileInputStream fis = new FileInputStream(outFile);
+                            while ((len = fis.read(buf)) > 0) {
+                                o.write(buf, 0, len);
+                            }
+                            resMessage=outFile.length()+";"+requestedFile+";"+s.getRemoteSocketAddress().toString()+";"+"Webcam";
+
+                        } else {
+                            File notFoundFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "404.html");
+                            res.write("HTTP/1.1 200 OK" + NEWLINE);
+                            res.write("Date: " + Calendar.getInstance().getTime() + NEWLINE);
+                            res.write("Server: localhost/12345" + NEWLINE);
+                            res.write("Content-Length: " + String.valueOf(notFoundFile.length()) + NEWLINE);
+                            res.write("Connection: Closed" + NEWLINE);
+                            res.write("Content-Type: text/html" + NEWLINE);
+                            res.write(NEWLINE);
+                            res.flush();
+                            byte[] buf = new byte[1024];
+                            int len = 0;
+                            FileInputStream fis = new FileInputStream(notFoundFile);
+                            while ((len = fis.read(buf)) > 0) {
+                                o.write(buf, 0, len);
+                            }
+                            resMessage=notFoundFile.length()+";"+requestedFile+";"+s.getRemoteSocketAddress().toString()+";"+"404 NOT FOUND";
+
+
+                            Log.d("SERVER", "File not found");
+                        }
+
+
+                        Message msg = messageHandler.obtainMessage();
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("REQUEST", (Serializable) resMessage);
+                        msg.setData(bundle);
+                        messageHandler.sendMessage(msg);
+
+
+                        s.close();
+                        Log.d("SERVER", "Socket Closed");
+                    }
+                }
+                   /* else if(request.Method.toUpperCase().equals("PUT"))
+                    {
+                        Log.d("SERVER","Put methode");
+
+                    }*/
+                else {
+                    Log.d("SERVER", "bad request methode!");
+                }
+
+            }
+        } catch (Exception e) {
+            Log.d("SERVER ERROR", e.toString());
+        } finally {
+            serverSocket = null;
+            bRunning = false;
+        }
+    }
+
+    private void makeCameraPhoto() throws Exception {
+        if (cameraManager == null) {
+            throw new Exception("NO CAMERA");
+        }
+
+        takePicture();
+
+    }
+
+    public void takePicture() {
+        try {
+            if (cameraManager == null) {
+                Log.d("takePictureFromCamera", "no camera hardware found");
+                // todo return info
+                return;
+            }
+
+
+            if (cameraInstance == null) {
+                Log.d("takePictureFromCamera", "could not get access to camera");
+                return;
+            }
+
+
+            cameraInstance.takePicture(null, null, this.cameraManager);
+        } catch (Exception e) {
+            Log.d("Take picture error: ", e.getLocalizedMessage());
+        }
+
+       /* try {
+            // sync time
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Log.d("takePictureFromCamera", "sync interrupted");
+        }*/
+
+        //cameraInstance.release();
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        if (this.streaming == true) {
+            try {
+                byte[] baos = convertYuvToJpeg(data, camera);
+
+                imageBuffer.reset();
+                imageBuffer.write(baos);
+                imageBuffer.flush();
+
+                stream.write((NEWLINE + boundary + NEWLINE +
+                        "Content-type: image/jpeg" + NEWLINE +
+                        "Content-Length: " + imageBuffer.size() + NEWLINE + NEWLINE).getBytes());
+
+                stream.write(imageBuffer.toByteArray());
+                stream.write((NEWLINE).getBytes());
+
+                stream.flush();
+            } catch (IOException e) {
+                Log.d("onPreviewFrame error:  ", e.getLocalizedMessage());
+            }
+        }
+    }
+
+    public byte[] convertYuvToJpeg(byte[] data, Camera camera) {
+
+        YuvImage image = new YuvImage(data, ImageFormat.NV21,
+                camera.getParameters().getPreviewSize().width, camera.getParameters().getPreviewSize().height, null);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int quality = 20;
+        image.compressToJpeg(new Rect(0, 0, camera.getParameters().getPreviewSize().width, camera.getParameters().getPreviewSize().height), quality, baos);//this line decreases the image quality
+
+
+        return baos.toByteArray();
+    }
+}
---
+++ app/src/main/java/com/kru13/httpserver/SocketServer.java
@@ -0,0 +1,143 @@
+package com.kru13.httpserver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import static android.content.ContentValues.TAG;
+
+public class SocketServer extends Thread {
+	
+	ServerSocket serverSocket;
+
+	Handler messageHandler;
+	CameraManager cameraManager;
+	Camera cameraInstance;
+	public final int port = 12345;
+	boolean bRunning;
+
+
+	public SocketServer(Handler handler, CameraManager cameraManager, Camera cameraInstance)
+	{
+		this.messageHandler = handler;
+		this.cameraManager = cameraManager;
+		this.cameraInstance = cameraInstance;
+	}
+
+
+
+
+	public void close() {
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			Log.d("SERVER", "Error, probably interrupted in accept(), see log");
+			e.printStackTrace();
+		}
+		bRunning = false;
+	}
+	
+	public void run() {
+		
+		try {
+
+        	Log.d("SERVER", "Creating Socket");
+			serverSocket = new ServerSocket(port);
+			ClientHandler client = new ClientHandler(serverSocket, messageHandler, cameraManager, cameraInstance);
+			client.run();
+			//client.join();
+
+		} catch (IOException e) {
+			if (serverSocket != null && serverSocket.isClosed())
+				Log.d("SERVER", "Normal exit");
+			else {
+				Log.d("SERVER", "Error");
+				e.printStackTrace();
+			}
+		}
+       /* try {
+            serverSocket = new ServerSocket(port);
+            bRunning = true;
+            while (bRunning) {
+            	Log.d("SERVER", "Socket Waiting for connection");
+                Socket s = serverSocket.accept(); 
+                Log.d("SERVER", "Socket Accepted");
+				File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+				StringBuilder res = new StringBuilder();
+				StringBuilder resContent = new StringBuilder();
+				OutputStream o = s.getOutputStream();
+	        	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(o));
+	        	BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+	            String req = in.readLine();
+				String[] reqData = req.split("\\s+");
+				String reqestType = reqData[0];
+				String requestedFile = reqData[1];
+				File file = null;
+				if(requestedFile.equalsIgnoreCase("/index.html")) {
+					file = new File(sdcard, "index.html");
+				}
+				else {
+					file = new File(sdcard, "not-found.html");
+				}
+
+					try {
+						BufferedReader br = new BufferedReader(new FileReader(file));
+						String line;
+
+						while ((line = br.readLine()) != null) {
+							resContent.append(line);
+							resContent.append('\n');
+						}
+						br.close();
+					}
+					catch (IOException e) {
+						//You'll need to add proper error handling here
+					}
+
+				res.append("HTTP/1.1 200 OK\n" +
+						"Date: Mon, 27 Jul 2009 12:28:53 GMT\n" +
+						"Server: Apache/2.2.14 (Win32)\n" +
+						"Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n" +
+						"Content-Type: text/html\n" +
+						"Connection: Closed\n\n");
+				res.append(resContent);
+	            out.write(res.toString());
+                out.flush();
+	            
+                s.close();
+                Log.d("SERVER", "Socket Closed");
+            }
+        }
+        catch (IOException e) {
+            if (serverSocket != null && serverSocket.isClosed())
+            	Log.d("SERVER", "Normal exit");
+            else {
+            	Log.d("SERVER", "Error");
+            	e.printStackTrace();
+            }
+        }
+        finally {
+        	serverSocket = null;
+        	bRunning = false;
+        }*/
+    }
+
+}
